de5688d3256dc0f74d981a54e69f3ddc64c89f90
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-19 04:19:32 -0700

    fix an error

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,7 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class chatRoomConsumer(AsyncWebsocketConsumer):
+class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
--- core/routing.py
+++ core/routing.py
@@ -1,6 +1,6 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
+import chatapp.routing
 
 
 application = ProtocolTypeRouter({
