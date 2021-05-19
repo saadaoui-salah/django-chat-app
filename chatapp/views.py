1421e4704a380cf28b9c7f929ffd2a18ac2ad2d6
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-19 05:38:16 -0700

    update room func

--- chatapp/views.py
+++ chatapp/views.py
@@ -6,7 +6,7 @@ def index(request):
     context  = {}
     return render(request, "index.html", context)
 
-def room(request):
-    context  = {}
+def room(request, room_name):
+    context  = {"room_name":room_name}
     return render(request, "index.html", context)
 
