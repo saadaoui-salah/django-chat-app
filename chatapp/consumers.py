dd9d1986ce3d8ce3945c26a848eb0b38e83f409a
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-19 12:11:10 -0700

    add username

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -28,17 +28,23 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         
     # Receiving msg from the client
     async def receive(self, text_data):
-        message_json = json.loads(text_data)
-        message = message_json['message']
+        data_json = json.loads(text_data)
+        message = data_json['message']
+        username = data_json['username']
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type':"chat_message",
-                'message':message
+                'message':message,
+                'username':username
             }
         )
 
     # send the message to the client
     async def chat_message(self, event):
         message = event['message']
-        await self.send(text_data=json.dumps({'message':message}))
+        username = event['username']
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username':username,
+            }))
